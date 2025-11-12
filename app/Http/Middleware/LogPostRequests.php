152dd4866b959f2d1cc61ba3374abbe29c9be0c1
Dimitri Boyko <n30nv4mp@yandex.ru>
2025-11-12 21:24:42 +0300

    feat: Add LogPostRequests middleware

---
+++ app/Http/Middleware/LogPostRequests.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as RequestAlias;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\EventLogger;
+
+class LogPostRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if ($request->isMethod(RequestAlias::METHOD_POST)) {
+            EventLogger::log(
+                $request->method() . ' ' . 'log',
+                'Automatic POST log',
+                $request->all()
+            );
+        }
+
+        return $next($request);
+    }
+}
