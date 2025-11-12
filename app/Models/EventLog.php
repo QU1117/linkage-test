86644aa7c64da918649cb9ef295b5f6ddfdfcc91
Dimitri Boyko <n30nv4mp@yandex.ru>
2025-11-12 21:23:11 +0300

    feat: Add EventLog model

---
+++ app/Models/EventLog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventLog extends Model
+{
+    protected $fillable = [
+        'type', 'description', 'request_data',
+    ];
+
+    protected $casts = [
+        'request_data' => 'array',
+    ];
+}
