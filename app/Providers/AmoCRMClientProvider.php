907613acab01cd1977698823969db8abc8582430
Dimitri Boyko <n30nv4mp@yandex.ru>
2025-11-16 20:33:13 +0300

    fix: Fix AmoCRM client initialization in service provider

--- app/Providers/AmoCRMClientProvider.php
+++ app/Providers/AmoCRMClientProvider.php
@@ -2,25 +2,39 @@
 
 namespace App\Providers;
 
+use AmoCRM\OAuth2\Client\Provider\AmoCRMException;
+use App\Services\EventLogger;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use AmoCRM\Client\AmoCRMApiClient;
+use League\OAuth2\Client\Token\AccessToken;
 
-class AmoCRMClientProvider extends ServiceProvider implements DeferrableProvider
+class AmoCRMClientProvider extends ServiceProvider
 {
     /**
      * Register services.
      */
     public function register(): void
     {
-        $this->app->singleton(AmoCRMApiClient::class, function () {
-            $apiClient = new AmoCRMApiClient(
-                config('services.amocrm.client_id'),
-                config('services.amocrm.client_secret'),
-                config('services.amocrm.redirect_uri'),
-            );
-            $apiClient->setAccessToken(config('services.amocrm.access_token'))
-                ->setAccountBaseDomain(config('services.amocrm.base_domain'));
+        $this->app->bind(AmoCRMApiClient::class, function ($app) {
+            try {
+                $apiClient = new AmoCRMApiClient(
+                    config('services.amocrm.client_id'),
+                    config('services.amocrm.client_secret'),
+                    config('services.amocrm.redirect_uri'),
+                );
+                $accessToken = new AccessToken([
+                    'access_token' => config('services.amocrm.access_token'),
+                    'refresh_token' => config('services.amocrm.refresh_token'),
+                    'expires_in' => env('AMOCRM_EXPIRES'),
+                ]);
+
+                $apiClient->setAccessToken($accessToken)
+                    ->setAccountBaseDomain(config('services.amocrm.base_domain'));
+            } catch (AmoCRMException $exception) {
+                Eventlogger::log('AmoCRM Client Error', 'AmoCRM client error while initializing in service provider',
+                    $exception->getMessage());
+            }
 
             return $apiClient;
         });
@@ -32,10 +46,5 @@ class AmoCRMClientProvider extends ServiceProvider implements DeferrableProvider
     public function boot(): void
     {
         //
-    }
-
-    public function provides(): array
-    {
-        return [AmoCRMApiClient::class];
     }
 }
