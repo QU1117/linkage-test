9e6d8e3344a879b2c2971a9a6abcea38428dbc41
Dimitri Boyko <n30nv4mp@yandex.ru>
2025-11-16 22:50:05 +0300

    refactor: refactor SiteWebhookController into chain call Jobs

--- app/Http/Controllers/SiteWebhookController.php
+++ app/Http/Controllers/SiteWebhookController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\EventLogger;
 use Illuminate\Http\Request;
 use App\Jobs\AmoAddLeadWithContactJob;
 use App\Jobs\SendTelegramNotification;
+use Illuminate\Support\Facades\Bus;
 
 class SiteWebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             'name' => 'required|string|max:255',
             'phone' => 'required|phone:RU',
             'apartment_address' => 'required|string|max:255',
         ]);
 
-        AmoAddLeadWithContactJob::dispatch();
-        SendTelegramNotification::dispatch();
+        Bus::chain([
+            new AmoAddLeadWithContactJob($validated),
+            new SendTelegramNotification($validated)
+        ])->dispatch();
     }
 }
